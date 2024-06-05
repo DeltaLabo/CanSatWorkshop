ac951aa338e2b01656a7448f5cd1a6f290067a49
anthonyarguedas <anthony_arguedas@outlook.com>
2024-06-05 17:01:09 -0600

    Create pin definitions file

---
+++ main/pins.h
@@ -0,0 +1,15 @@
+#ifndef PINS
+#define PINS
+
+/******* Begin Comms Pins *******/
+#define LORA_TX_PIN D0
+#define LORA_RX_PIN D1
+/******* End Comms Pins *******/
+
+/******* Begin Sensors Pins *******/
+/******* End Sensors Pins *******/
+
+/******* Begin Position and Orientation Pins *******/
+/******* End Position and Orientation Pins *******/
+
+#endif

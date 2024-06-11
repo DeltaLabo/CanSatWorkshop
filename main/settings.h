1f840a501029fb8e7ef5c8c0725b0e1f66c06dc9
anthonyarguedas <anthony_arguedas@outlook.com>
2024-06-11 10:14:13 -0600

    Add GS periods

--- main/settings.h
+++ main/settings.h
@@ -3,34 +3,43 @@
 
 /******* Begin Comms Settings *******/
 // Header to identify LoRa packages out of all possible LoRa comms in the area
-#define LORA_HEADER "CSWS" // [C]an[S]at [W]ork[s]hop
+#define LORA_HEADER String("CSWS") // [C]an[S]at [W]ork[s]hop
 
 // Footer to check message integrity
-#define LORA_FOOTER "DTLB" // [D]el[t]a [L]a[b]
+#define LORA_FOOTER String("DTLB") // [D]el[t]a [L]a[b]
 
 // Unique ID of the CanSat (2 alphanumeric characters)
-#define CANSAT_LORA_ID "M1"
+#define CANSAT_LORA_ID String("M1")
 
 // Unique ID of the ground station                               
-#define GS_LORA_ID "GS"
+#define GS_LORA_ID String("GS")
 
 // Ground station TX request command
-#define LORA_TX_COMMAND "DATA"
+#define LORA_TX_COMMAND String("DATA")
+
+// Data request expected from ground station
+#define TXRequest String(LORA_HEADER + GS_LORA_ID + CANSAT_LORA_ID + LORA_TX_COMMAND + LORA_FOOTER)
 
 // LoRa frequency band
 // 915 MHz for Costa Rica and Panama
-#define LORA_BAND "915" // MHz
+#define LORA_BAND String("915") // MHz
 
 // Spreading Factors 5 to 11 are supported
-#define LORA_SF "5"
+#define LORA_SF String("7")
 
 // 7: 125 kHz (SF7 to SF9)
 // 8: 250 kHz (SF7 to SF10)
 // 9: 500 kHz (SF7 to DF11)
-#define LORA_BANDWIDTH "7"
+#define LORA_BANDWIDTH String("7")
 
 // Coding rates 1 to 4 are supported
-#define LORA_CODING_RATE "1"
+#define LORA_CODING_RATE String("1")
+
+// Period between ground station TX requests
+#define GS_TX_REQUEST_PERIOD 1000 // ms
+
+// Time that the ground station waits for a response after a TX Request
+#define GS_LISTEN_PERIOD 500 // ms
 /******* End Comms Settings *******/
 
 /******* Begin Sensor Settings *******/

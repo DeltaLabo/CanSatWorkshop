a3b04ead1dcd19853f17fdd0136d118f5e230af4
anthonyarguedas <anthony_arguedas@outlook.com>
2024-06-11 18:09:40 -0600

    Move RYLR GS to tests

---
+++ tests/rylr_ground_station/pins.h
@@ -0,0 +1,15 @@
+#ifndef PINS
+#define PINS
+
+/******* Begin Comms Pins *******/
+#define LORA_TX_PIN 5//D0
+#define LORA_RX_PIN 4//D1
+/******* End Comms Pins *******/
+
+/******* Begin Sensor Pins *******/
+/******* End Sensor Pins *******/
+
+/******* Begin Position and Orientation Pins *******/
+/******* End Position and Orientation Pins *******/
+
+#endif

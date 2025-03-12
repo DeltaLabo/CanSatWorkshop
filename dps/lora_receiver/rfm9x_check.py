a190c66a53c7d11a604e1d21f5907b11363bbed9
anthonyarguedas <anthony_arguedas@outlook.com>
2025-03-12 10:16:44 -0600

    Port to Orange Pi

--- dps/lora_receiver/rfm9x_check.py
+++ dps/lora_receiver/rfm9x_check.py
@@ -3,81 +3,70 @@
 # SPDX-License-Identifier: MIT
 
 """
-Wiring Check, Pi Radio w/RFM9x
+Wiring Check, Orange Pi w/RFM9x
 
-Learn Guide: https://learn.adafruit.com/lora-and-lorawan-for-raspberry-pi
-Author: Brent Rubell for Adafruit Industries
+Learn Guide: Modified from https://learn.adafruit.com/lora-and-lorawan-for-raspberry-pi
+Adapted for Orange Pi
 """
 import time
-import busio
-from digitalio import DigitalInOut, Direction, Pull
-import board
-# Import the SSD1306 module.
-import adafruit_ssd1306
-# Import the RFM9x radio module.
-import adafruit_rfm9x
+import spidev
+import OPi.GPIO as GPIO
 
-# Button A
-btnA = DigitalInOut(board.D5)
-btnA.direction = Direction.INPUT
-btnA.pull = Pull.UP
-
-# Button B
-btnB = DigitalInOut(board.D6)
-btnB.direction = Direction.INPUT
-btnB.pull = Pull.UP
-
-# Button C
-btnC = DigitalInOut(board.D12)
-btnC.direction = Direction.INPUT
-btnC.pull = Pull.UP
-
-# Create the I2C interface.
-i2c = busio.I2C(board.SCL, board.SDA)
-
-# 128x32 OLED Display
-reset_pin = DigitalInOut(board.D4)
-display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
-# Clear the display.
-display.fill(0)
-display.show()
-width = display.width
-height = display.height
+# Configure GPIO settings
+GPIO.setboard(GPIO.ORANGEPI_3)
+GPIO.setmode(GPIO.BOARD)
 
 # Configure RFM9x LoRa Radio
-CS = DigitalInOut(board.CE1)
-RESET = DigitalInOut(board.D25)
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+CS_PIN = 15
+RESET_PIN = 14
 
-while True:
-    # Clear the image
-    display.fill(0)
+# Setup GPIO
+GPIO.setup(CS_PIN, GPIO.OUT)
+GPIO.setup(RESET_PIN, GPIO.OUT)
 
-    # Attempt to set up the RFM9x Module
-    try:
-        rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
-        display.text('RFM9x: Detected', 0, 0, 1)
-    except RuntimeError as error:
-        # Thrown on version mismatch
-        display.text('RFM9x: ERROR', 0, 0, 1)
-        print('RFM9x Error: ', error)
+# Initialize SPI
+spi = spidev.SpiDev()
+spi.open(0, 0)  # SPI bus 0, device 0
+spi.max_speed_hz = 5000000  # 5MHz
 
-    # Check buttons
-    if not btnA.value:
-        # Button A Pressed
-        display.text('Ada', width-85, height-7, 1)
-        display.show()
-        time.sleep(0.1)
-    if not btnB.value:
-        # Button B Pressed
-        display.text('Fruit', width-75, height-7, 1)
-        display.show()
-        time.sleep(0.1)
-    if not btnC.value:
-        # Button C Pressed
-        display.text('Radio', width-65, height-7, 1)
-        display.show()
-        time.sleep(0.1)
+# Create digital IO objects for the CS and RESET pins
+class DigitalInOut:
+    def __init__(self, pin):
+        self.pin = pin
+        self.switch_to_output(True)
+    
+    def switch_to_output(self, value=False):
+        GPIO.output(self.pin, GPIO.HIGH if value else GPIO.LOW)
+        self.value = value
 
-    display.show()
-    time.sleep(0.1)
+cs = DigitalInOut(CS_PIN)
+reset = DigitalInOut(RESET_PIN)
+
+# Import the RFM9x radio module - do this after GPIO setup
+import orangepi_rfm9x
+
+try:
+    # Initialize the RFM9x module
+    rfm9x = orangepi_rfm9x.RFM9x(spi, cs, reset, 915.0)
+    print("RFM9x: Detected")
+    
+    # Basic test of sending and receiving
+    print("Sending test packet...")
+    rfm9x.send(bytes("Hello Orange Pi!", "utf-8"))
+    print("Waiting for packets...")
+
+    # Wait for a packet
+    packet = rfm9x.receive(timeout=5.0)
+    if packet is not None:
+        print("Received: ", packet)
+        print("RSSI: ", rfm9x.last_rssi)
+    else:
+        print("No packet received")
+        
+except RuntimeError as error:
+    # Thrown on version mismatch
+    print(f"RFM9x Error: {error}")
+
+# Clean up
+spi.close()
+GPIO.cleanup()
